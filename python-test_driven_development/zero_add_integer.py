251bd6256760f8f2b8074cc5e2c88d7ccf3c5438
13590-pablus <13590@holbertonstudents.com>
2026-03-20 19:26:16 +0000

    FF

--- python-test_driven_development/zero_add_integer.py
+++ python-test_driven_development/zero_add_integer.py
@@ -7,19 +7,12 @@ This module defines a function that adds two integers.
 def add_integer(a, b=98):
     """
     Adds two integers (or floats casted to integers).
-
-    Args:
-        a: int or float
-        b: int or float (default 98)
-
-    Returns:
-        int: sum of a and b
-
-    Raises:
-        TypeError: if a or b is not int or float
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
