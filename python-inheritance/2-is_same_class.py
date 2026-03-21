756e78331cb0ba1b52a71567eeb1478e4cb1b347
13590-pablus <13590@holbertonstudents.com>
2026-03-21 06:56:00 +0000

    2. Exact same object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -12,6 +12,4 @@ def is_same_class(obj, a_class):
         If type(obj) == a_class - True.
         Otherwise - False.
     """
-    if type(obj) is a_class:
-        return True
-    return False
+    return type(obj) is a_class
