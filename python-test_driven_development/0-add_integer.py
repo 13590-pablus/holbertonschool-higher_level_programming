092bbfe46b435565f6f209631f7b8ae1fef811a9
13590-pablus <13590@holbertonstudents.com>
2026-04-03 20:13:06 +0000

    Fix doctests to catch Float overflow, NaN and missing arguments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,16 +3,17 @@
 This module contains a function that adds two integers.
 """
 
+
 def add_integer(a, b=98):
     """Adds two integers or floats.
-    
+
     Args:
         a: first number
         b: second number, defaults to 98
-        
+
     Returns:
         The addition of a and b as an integer.
-        
+
     Raises:
         TypeError: If a or b is not an integer or float.
     """
@@ -20,5 +21,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-        
+
     return int(a) + int(b)
