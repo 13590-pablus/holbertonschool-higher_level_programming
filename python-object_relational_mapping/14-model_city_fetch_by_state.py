00c09750fdcf0a79ffb727aeeb5b5726d1c692f5
13590-pablus <13590@holbertonstudents.com>
2026-04-11 13:00:22 +0000

    Fix pycodestyle issues for task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,16 +10,12 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Engine yaradılır
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # City və State cədvəllərini birləşdirib (JOIN) arqumentdəki ştatı axtarırıq
-    # Lakin Desired Output bütün ştatları göstərir, deməli biz 
-    # bütün şəhərləri ştat adları ilə birgə götürməliyik.
     results = session.query(City, State).join(State, City.state_id == State.id)\
                      .order_by(City.id).all()
 
