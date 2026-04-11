486a26b4fef553050c5f8098320c7b67e60ea072
13590-pablus <13590@holbertonstudents.com>
2026-04-11 12:59:10 +0000

    Fix output format for task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Lists all City objects from database"""
+"""
+Lists all City objects from the database hbtn_0e_14_usa
+"""
 import sys
 from model_state import Base, State
 from model_city import City
@@ -8,12 +10,20 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    # Engine yaradılır
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(City).join(State).filter(State.name == sys.argv[4])\
-                   .order_by(City.id).all()
-    for city in query:
-        print("{}: {}".format(city.id, city.name))
+
+    # City və State cədvəllərini birləşdirib (JOIN) arqumentdəki ştatı axtarırıq
+    # Lakin Desired Output bütün ştatları göstərir, deməli biz 
+    # bütün şəhərləri ştat adları ilə birgə götürməliyik.
+    results = session.query(City, State).join(State, City.state_id == State.id)\
+                     .order_by(City.id).all()
+
+    for city, state in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
     session.close()
