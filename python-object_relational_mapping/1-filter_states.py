b86b95945b8851650cf526f77a75dd39997b73f6
13590-pablus <13590@holbertonstudents.com>
2026-04-11 12:52:16 +0000

    Add script to filter states by N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Verilənlər bazasına qoşulma
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Kursor yaradılır
+    cursor = db.cursor()
+
+    # SQL sorğusu: Adı 'N' ilə başlayanları seç (Böyük-kiçik hərf həssaslığı üçün BINARY)
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    # Nəticələr götürülür
+    rows = cursor.fetchall()
+
+    # Çap edilir
+    for row in rows:
+        print(row)
+
+    # Bağlantılar bağlanır
+    cursor.close()
+    db.close()
